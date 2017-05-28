caba1090761ac18e87f9896813039465b7ab5276
David Sutherland <david@wwwebbify.com>
2017-05-27 23:10:11 -0600

    Create setStyles
    initial creation of setStyles

---
+++ setSytles.js
@@ -0,0 +1,19 @@
+/*
+    @public
+    @function setStyles
+    @description - Add styles for specific target element (will not destroy other inline styles)
+    @param {object or array of objects} [element] - The specified element or array of elements
+    @param {object} [styles] - An object of styles in CSS String format to apply to element
+    @param {boolean} keep - Boolean to keep current inline styles(true) or to overwrite them(false - default)
+*/
+function setStyles(element, styles, keep = false) {
+    element = Array.isArray(element) ? element : [element];
+    element.forEach(function (element) {
+        var styleString = keep ? element.getAttribute("style") : '';
+        for (var prop in styles) {
+            styleString += prop + ':' + styles[prop] + ';'
+        }
+        element.setAttribute("style", styleString);
+    }
+    );
+}
